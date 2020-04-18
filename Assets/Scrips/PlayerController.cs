8cf4bbe1121849a24239e4b4795dd8db3adabd21
ZachZ <ZachZ@users.noreply.github.com>
2020-04-18 05:40:42 -0600

    Some animation

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float pSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * pSpeed * Time.fixedDeltaTime);
+
+    }
+
 }
